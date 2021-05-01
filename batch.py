52db518cebaace21c5385aef339605657c55d307
riyadparvez <riyad@ukko.ag>
2021-05-01 07:53:23 -0400

    Batch three days earlier

--- batch.py
+++ batch.py
@@ -11,14 +11,14 @@ auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
 auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
-def f():
+def batch_sync():
     today = datetime.date.today()
-    yesterday= today - datetime.timedelta(days=2)
+    start_time= today - datetime.timedelta(days=3)
 
-    tweets_list = tweepy.Cursor(api.search, q=f"from:vaxhunterscan since: {yesterday} until: {today}", tweet_mode='extended', lang='en').items()
+    tweets_list = tweepy.Cursor(api.search, q=f"from:vaxhunterscan since: {start_time} until: {today}", tweet_mode='extended', lang='en').items()
     tweets_list = tweepy.Cursor(api.search, q=f"from:vaxhunterscan", tweet_mode='extended', lang='en').items()
     output = []
     for tweet_obj in tweets_list:
         process_tweet(tweet_obj)
 
-f()
+batch_sync()
