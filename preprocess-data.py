4a78da2579cebd13d7faa55db627f3e73213cedf
riyadparvez <riyad@ukko.ag>
2021-05-04 12:01:59 -0400

    Added suppoort for mapping FSA to city

--- preprocess-data.py
+++ preprocess-data.py
@@ -25,9 +25,22 @@ with open('FSA.pickle', 'wb') as handle:
 
 df = pd.read_csv('canadacities.csv')
 df['city'] = df['city'].str.lower()
+df['postal'] = df['postal'].str.split()
 print(df)
 
 cities = set(df['city'].values.tolist())
 print(cities)
 with open('cities.pickle', 'wb') as handle:
     pickle.dump(cities, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+cities_fsas = df[['city_ascii', 'postal']].values.tolist()
+print(cities_fsas)
+
+fsa_city_dict = {}
+for city, fsas in cities_fsas:
+    for fsa in fsas:
+        fsa_city_dict[fsa] = city
+print(fsa_city_dict)
+with open('fsa-city-dict.pickle', 'wb') as handle:
+    pickle.dump(fsa_city_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
